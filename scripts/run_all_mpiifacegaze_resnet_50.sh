d542adea9639f383978ac05127de4ffcbe964657
Kevin <koolkev246@gmail.com>
2023-11-30 03:50:57 -0500

    fixed 50 script

--- scripts/run_all_mpiifacegaze_resnet_50.sh
+++ scripts/run_all_mpiifacegaze_resnet_50.sh
@@ -6,15 +6,16 @@ arch=resnet_50
 exp_id=${1:-00}
 devices=${2:-0}
 exp_root_dir=experiments/mpiifacegaze/${arch}/exp${exp_id}/
-test_id=0
-CUDA_VISIBLE_DEVICES=${devices} python -u train.py \
-    --config configs/mpiifacegaze/${arch}_train.yaml \
-    train.test_id ${test_id} \
-    train.output_dir "${exp_root_dir}"
+for test_id in {0..14}; do
+    CUDA_VISIBLE_DEVICES=${devices} python -u train.py \
+        --config configs/mpiifacegaze/${arch}_train.yaml \
+        train.test_id ${test_id} \
+        train.output_dir "${exp_root_dir}"
 
-exp_dir=${exp_root_dir}/$(printf %02d ${test_id})
-CUDA_VISIBLE_DEVICES=${devices} python -u evaluate.py \
-    --config configs/mpiifacegaze/${arch}_eval.yaml \
-    test.test_id ${test_id} \
-    test.checkpoint "${exp_dir}"/checkpoint_0015.pth \
-    test.output_dir "${exp_dir}"/eval
+    exp_dir=${exp_root_dir}/$(printf %02d ${test_id})
+    CUDA_VISIBLE_DEVICES=${devices} python -u evaluate.py \
+        --config configs/mpiifacegaze/${arch}_eval.yaml \
+        test.test_id ${test_id} \
+        test.checkpoint "${exp_dir}"/checkpoint_0015.pth \
+        test.output_dir "${exp_dir}"/eval
+done
